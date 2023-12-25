e92b65045d25148e7696fadfc00df31d65ddbb47
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-12-25 13:58:58 -0500

    Types

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
+interface Data {
+  username: string;
+  password: string;
+}
+
+type RequestState =
+  | { status: "idle" }
+  | { status: "loading" }
+  | { status: "success"; data: any }
+  | { status: "error"; error: Error };
+
 export default function Home() {
-  return <div>Hey</div>;
+  const [data, setData] = useState<Data>({
+    username: "",
+    password: "",
+  });
+
+  const [requestState, setRequestState] = useState<RequestState>({
+    status: "idle",
+  });
+
+  function handleData() {}
+
+  return (
+    <div className=" h-screen w-1/3 bg-black flex flex-col justify-center items-center mx-auto">
+      <input type="text" placeholder="User..." />
+
+      <input type="text" placeholder="Password..." />
+
+      <br />
+
+      {data.username}
+      <br />
+      {data.password}
+    </div>
+  );
 }
